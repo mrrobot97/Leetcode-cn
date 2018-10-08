c0411a40221dd417ea4a29e9ef56941703149d56
杨佳伟 <944750043@qq.com>
2018-10-08 19:56:41 +0800

    add question 462

---
+++ src/Question462.java
@@ -0,0 +1,30 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Question462 {
+
+
+    public static void main(String[] args){
+        int[] a=new int[]{1,3,2};
+        System.out.println(minMoves2(a));
+    }
+
+    //https://leetcode-cn.com/problems/minimum-moves-to-equal-array-elements-ii/description/
+    //hint:排序后寻找最中间的数x，对于a,b，若有a<x && b>x ,则|a-x|+|b-x|=b-a为固定值，因此位于x两边的数可以两两消除，因为他们变为x
+    //的步数之和为固定值b-a，对于c=x，则无需变动,因此寻找排序后最中间的数即可。
+    public static int minMoves2(int[] nums) {
+        List<Integer> list=new ArrayList();
+        for(int i:nums){
+            list.add(i);
+        }
+        Collections.sort(list);
+        int n=list.size();
+        int diff=0;
+        int mid=list.get(n/2);
+        for (int i = 0; i < n; i++) {
+            diff+=Math.abs(nums[i]-mid);
+        }
+        return diff;
+    }
+}
